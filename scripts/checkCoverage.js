4b2af490a0d137327189ef777257fc3f610b1455
Adson Gomes Oliveira <adsonprofessionalemail@gmail.com>
2024-01-16 21:55:29 -0300

    setup: Add config files

---
+++ scripts/checkCoverage.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-console */
+// const summary = require('../coverage/coverage-summary.json');
+
+// function checkCoverage() {
+//   const summaryTotal = summary.total;
+
+//   const summaryPercentages = [
+//     summaryTotal.lines.pct,
+//     summaryTotal.statements.pct,
+//     summaryTotal.functions.pct,
+//     summaryTotal.branches.pct
+//   ];
+
+//   summaryPercentages.forEach((spec) => {
+//     if (spec < 100) {
+//       throw new Error('Some coverage point is below 100');
+//     }
+//   });
+
+//   console.log('Well Done, all the coverage points are above or equal 100%');
+// }
+
+// checkCoverage();
+
+function temp() {}
+temp();
